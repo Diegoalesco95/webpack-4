d50ab8e9b2ceb53b78bbf0579be385ab60113e4f
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-02 09:15:13 -0500

    Trabajando con React

--- react/src/js/components/app.js
+++ react/src/js/components/app.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data.json';
+import Loader from './loader.js';
 
 function App() {
-  return <div>React is alive!</div>;
+  const [loaderList, setLoaderList] = useState([]);
+  function handleClick() {
+    setLoaderList(data.loaders);
+  }
+  return (
+    <div>
+      ¡React is alive!
+      <ul>
+        {loaderList.map(item => (
+          <Loader {...item} key={item.id} />
+        ))}
+      </ul>
+      <button onClick={handleClick}>Mostrar</button>
+    </div>
+  );
 }
 
 export default App;
